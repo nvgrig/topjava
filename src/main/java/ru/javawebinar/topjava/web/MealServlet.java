cdb285ddde8e0c0c5ac12e4058ded4a4e1a8d431
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-06-06 19:45:51 +0400

    meals view is done

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,12 +1,16 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalTime;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -17,7 +21,9 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        List<Meal> meals = MealsUtil.createListOfMeals();
+        request.setAttribute("mealsTo", MealsUtil.filteredByStreams(meals,
+                LocalTime.MIN, LocalTime.MAX, MealsUtil.CALORIES_PER_DAY));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,6 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page import="ru.javawebinar.topjava.util.TimeUtil" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8"%>
 <html lang="ru">
 <head>
     <title>Meals</title>
@@ -7,5 +9,24 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<hr>
+<table border="1" cellpadding="8" cellspacing="0">
+    <tr>
+        <th>Date</th>
+        <th>Description</th>
+        <th>Calories</th>
+        <th></th>
+        <th></th>
+    </tr>
+    <jsp:useBean id="mealsTo" scope="request" type="java.util.List"/>
+    <c:forEach items="${mealsTo}" var="mealTo">
+        <jsp:useBean id="mealTo" type="ru.javawebinar.topjava.model.MealTo"/>
+        <tr style="color:<%=mealTo.isExcess()?"red":"green"%>">
+            <td><%=mealTo.getDateTime().format(TimeUtil.DATE_TIME_FORMATTER)%></td>
+            <td><%=mealTo.getDescription()%></td>
+            <td><%=mealTo.getCalories()%></td>
+        </tr>
+    </c:forEach>
+</table>
 </body>
 </html>
