992149e3b990df9ebb136ce3d83270d6281f24c1
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-06-13 19:17:16 +0400

    MealService & MealRestController implemented

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,44 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
 public class MealRestController {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
     private MealService service;
 
+    public Collection<Meal> getAll() {
+        log.info("getAll");
+        return service.getAll();
+    }
+
+    public Meal get(int id) {
+        log.info("get {}", id);
+        return service.get(id);
+    }
+
+    public Meal create(Meal meal) {
+        log.info("create {}", meal);
+        checkNew(meal);
+        return service.create(meal);
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id);
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {} with id={}", meal, id);
+        assureIdConsistent(meal, id);
+        service.update(meal);
+    }
 }
