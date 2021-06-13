54ab6306ced20bc9726e92011dbd6dfc378d0224
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-06-13 19:28:09 +0400

    Meal added to Spring

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava.web.meal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
 
@@ -10,9 +12,11 @@ import java.util.Collection;
 import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
 
+@Controller
 public class MealRestController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
 
+    @Autowired
     private MealService service;
 
     public Collection<Meal> getAll() {
