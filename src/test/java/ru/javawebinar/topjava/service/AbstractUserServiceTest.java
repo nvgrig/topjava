c44e9720c84b678b9a654e3ca0dc9fce7b328d0a
Grigoryev Nikita <nvgrigoryev@gmail.com>
2021-07-11 13:09:39 +0400

    HW06 1.2 fixed

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -29,7 +29,7 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
     @Autowired
     private CacheManager cacheManager;
 
-    @Autowired
+    @Autowired(required = false)
     protected JpaUtil jpaUtil;
 
     @Before
